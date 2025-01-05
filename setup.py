d6e210cee15486b77ba9d3395e33f185527a910b
NikhitaK27 <knikhitasai27@gmail.com>
2025-01-05 18:08:35 +0530

    folder stucture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='NikhitaK27',
+    author_email='knikhitasai27@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    
+)
